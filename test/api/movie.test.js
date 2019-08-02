45d9b1f33f902a63396af62ebbe1450b5a4b251d
Alper <31041738+4ppe@users.noreply.github.com>
2019-08-02 23:30:15 +0300

    /api/movies POST test

--- test/api/movie.test.js
+++ test/api/movie.test.js
@@ -34,5 +34,33 @@ describe('/api/movies tests',() => {
                     done();
                 })
         })
-    })
+    });
+    describe('/POST movie',() => {
+        it('it should POST a movie',(done) => {
+            const movie = {
+                title: 'Test',
+                director_id : "5d41a0e69624c22b4cfc4b44",
+                category: 'test_category',
+                country: 'test_country',
+                year: 1950,
+                imdb_score: 9
+            };
+
+            chai.request(server)
+                .post('/api/movies')
+                .send(movie)
+                .set('x-access-token',token)
+                .end((err,res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('title');
+                    res.body.should.have.property('director_id');
+                    res.body.should.have.property('category');
+                    res.body.should.have.property('country');
+                    res.body.should.have.property('year');
+                    res.body.should.have.property('imdb_score');
+                    done();
+                })
+        });
+    });
 });
