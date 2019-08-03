c7bb5c16bcf89e0ca4149512bb1395099a5fdbff
Alper <31041738+4ppe@users.noreply.github.com>
2019-08-03 06:02:19 +0300

    /PUT/:director_id director test

--- test/api/director.test.js
+++ test/api/director.test.js
@@ -76,4 +76,27 @@ describe('/api/directors tests',() => {
             });
         });
     });
+
+    describe('/PUT/:director_id director',() => {
+        it('it should UPDATE a director given by id',(done) => {
+            const test_director = {
+                name: 'PUT_test_director_name',
+                surname: 'PUT_test_director_surname',
+                bio: 'PUT_test_director_biooooooooooooooooooooooooooooooooooooooooooo'
+            };
+
+            chai.request(server)
+            .put('/api/directors/'+directorId)
+            .send(test_director)
+            .set('x-access-token',token)
+            .end((err,res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('name').eql(test_director.name);
+                res.body.should.have.property('surname').eql(test_director.surname);
+                res.body.should.have.property('bio').eql(test_director.bio);
+                done();
+            });
+        });
+    });
 });
